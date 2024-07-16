1ba6c5dc9e746dd9c8756b53d6649ba1adb9081d
NSSKMIT <161367260+NSSKMIT@users.noreply.github.com>
2024-07-17 04:11:57 +0530

    Update login-page.js

--- js/login-page.js
+++ js/login-page.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.getElementById("login-form");
-    const loginButton = document.getElementById("login-form-submit");
-    const loginErrorMsg = document.getElementById("login-error-msg");
-  
-    loginButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      const username = loginForm.username.value;
-      const password = loginForm.password.value;
-  
-      if (username === "nsskmitofficial@gmail.com" && password === "NSSKMIT@ServiceforNation") {
-        alert("You have successfully logged in.");
-        // Store login state in local storage
-        localStorage.setItem("loggedIn", "true");
-        // Redirect to the protected page
-        window.location.href = "events.html"; // Change this to your protected page's URL
-      } else {
-        loginErrorMsg.style.opacity = 1;
-      }
-    });
+  const loginForm = document.getElementById("login-form");
+  const loginButton = document.getElementById("login-form-submit");
+  const loginErrorMsg = document.getElementById("login-error-msg");
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "nsskmitofficial@gmail.com" && password === "NSSKMIT") {
+      alert("You have successfully logged in.");
+      // Store login state in local storage
+      localStorage.setItem("loggedIn", "true");
+      // Redirect to the protected page
+      window.location.href = "events.html"; // Change this to your protected page's URL
+    } else {
+      loginErrorMsg.style.opacity = 1;
+    }
   });
-  
+});
